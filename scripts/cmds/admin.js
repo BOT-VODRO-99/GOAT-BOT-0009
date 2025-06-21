1330b86b4a7d9003c6314c9b46465c4af7cd296f
[👨🏿‍🌾]•𝑬𝒘`𝒓 𝑨𝒓𝒂𝒇𝒂𝒕 <mohammada4ara3@gmail.com>
2025-06-21 17:14:07 +0600

    Create admin.js

---
+++ scripts/cmds/admin.js
@@ -0,0 +1,121 @@
+const { config } = global.GoatBot;
+const { writeFileSync } = require("fs-extra");
+
+module.exports = {
+    config: {
+        name: "admin",
+        aliases: ["ad"],
+        version: "0",
+        author: "︎AHMED TARIF",
+        countDown: 5,
+        role: 0,
+        shortDescription: "Add, remove or see the admin list for this bot",
+        longDescription: "Add, remove or see the admin list for this bot",
+        category: "admin",
+        guide:  "   {pn} [list | -l]: Show admin list (everyone can use)\n" +
+                "   {pn} [add | -a] <uid | @tag>: Add admin role for a user (admins only)\n" +
+                "   {pn} [remove | -r] <uid | @tag>: Remove admin role from a user (admins only)\n" +
+                "   {pn} [add | -a, remove | -r] (reply): Add/remove admin role for the user you replied to (admins only)"
+        
+    },
+
+    langs: {
+        en: {
+            listAdmin: "👑 | 𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧𝐬 & 𝐎𝐩𝐞𝐫𝐚𝐭𝐨𝐫𝐬 | 👑"
+                + "\n ___________________"
+                + "\n♕︎| 	𝐎𝐖𝐍𝐄𝐑\n____________\n ⌬| ︎MOHAMMAD ARAFAT"
+                +"\n╰=> 61552422054139"
+                + "\n _____________________________"
+                + "\n♲︎︎︎| 𝐎𝐩𝐞𝐫𝐚𝐭𝐨𝐫𝐬"
+                +"\n____________"
+                + "\n %1"
+                + "\n _____________________________"
+                + "\n"
+                + "\n ",
+            noAdmin: "⚠ | No admins found!",
+            added: "✅ | Added admin role for %1 users:\n%2",
+            alreadyAdmin: "\n⚠ | %1 users already have admin role:\n%2",
+            missingIdAdd: "⚠ | Please provide an ID, tag a user, or reply to a message to add admin role",
+            removed: "✅ | Removed admin role from %1 users:\n%2",
+            notAdmin: "⚠ | %1 users do not have admin role:\n%2",
+            missingIdRemove: "⚠ | Please provide an ID, tag a user, or reply to a message to remove admin role",
+            notAllowed: "⛔ | You don't have permission to use this command!"
+        }
+    },
+
+    onStart: async function ({ message, args, usersData, event, getLang }) {
+        const senderID = event.senderID;
+
+        switch (args[0]) {
+            case "list":
+            case "-l": {
+                
+                if (config.adminBot.length === 0) {
+                    return message.reply(getLang("noAdmin"));
+                }
+                const getNames = await Promise.all(config.adminBot.map(uid => usersData.getName(uid).then(name => `⌬| ${name} \n╰=>${uid}`)));
+                return message.reply(getLang("listAdmin", getNames.join("\n")));
+            }
+
+            case "add":
+            case "-a":
+            case "remove":
+            case "-r": {
+                
+                if (!config.adminBot.includes(senderID)) {
+                    return message.reply(getLang("notAllowed"));
+                }
+            }
+
+            if (args[0] === "add" || args[0] === "-a") {
+                let uids = [];
+
+                
+                if (Object.keys(event.mentions).length > 0) {
+                    uids = Object.keys(event.mentions);
+                } else if (event.type === "message_reply") {
+                    uids.push(event.messageReply.senderID);
+                } else {
+                    uids = args.filter(arg => !isNaN(arg));
+                }
+
+                if (uids.length === 0) {
+                    return message.reply(getLang("missingIdAdd"));
+                }
+
+                const newAdmins = [];
+                const alreadyAdmins = [];
+
+                for (const uid of uids) {
+                    if (config.adminBot.includes(uid)) {
+                        alreadyAdmins.push(uid);
+                    } else {
+                        newAdmins.push(uid);
+                    }
+                }
+
+                config.adminBot.push(...newAdmins);
+                writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
+
+                const newAdminNames = await Promise.all(newAdmins.map(uid => usersData.getName(uid)));
+                const alreadyAdminNames = await Promise.all(alreadyAdmins.map(uid => usersData.getName(uid)));
+
+                return message.reply(
+                    (newAdmins.length > 0 ? 
+                        getLang("added", newAdmins.length, newAdminNames.map(name => `• ${name}`).join("\n")) : "") +
+                    (alreadyAdmins.length > 0 ? 
+                        getLang("alreadyAdmin", alreadyAdmins.length, alreadyAdminNames.map(name => `• ${name}`).join("\n")) : "")
+                );
+            }
+
+            if (args[0] === "remove" || args[0] === "-r") {
+                let uids = [];
+
+                
+                if (Object.keys(event.mentions).length > 0) {
+                    uids = Object.keys(event.mentions);
+                } else if (event.type === "message_reply") {
+                    uids.push(event.messageReply.senderID);
+                } else {
+                    uids = args.filter(arg => !isNaN(arg));
+                }
